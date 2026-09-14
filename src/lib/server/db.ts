78beec8c954b342ef06687e6ac42583ca8d64812
Cedric Greiten <me@cegre.dev>
2026-09-15 01:59:16 +0200

    docker setup working with drizzle

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,16 +1,23 @@
 import fs from "node:fs";
 import path from "node:path";
 
-const DB_PATH = process.env.DATABASE_PATH || "data/db.sqlite3";
+const DB_PATH = process.env.DATABASE_PATH ?? "data/db.sqlite3";
+const MIGRATIONS_PATH = process.env.MIGRATIONS_PATH ?? "drizzle";
 
 fs.mkdirSync(path.dirname(DB_PATH), { recursive: true });
 
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
+import { migrate } from "drizzle-orm/libsql/migrator";
+
 import { authRelations, relations } from "./db/relations";
 
 // You can specify any property from the libsql connection options
 export const db = drizzle({
-	connection: { url: process.env.DB_FILE_NAME! },
+	connection: { url: "file:" + DB_PATH },
 	relations: { ...relations, ...authRelations },
+});
+
+await migrate(db, {
+	migrationsFolder: MIGRATIONS_PATH,
 });
