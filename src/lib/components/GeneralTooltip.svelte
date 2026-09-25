a6475daedd8924d4b596c322b45e253c2ba3b223
Cedric Greiten <me@cegre.dev>
2026-09-25 22:31:58 +0200

    add tooltip

---
+++ src/lib/components/GeneralTooltip.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Tooltip } from "bits-ui";
+	import type { Snippet } from "svelte";
+
+	let { trigger, content }: { trigger: Snippet; content: Snippet } = $props();
+</script>
+
+<Tooltip.Provider>
+	<Tooltip.Root delayDuration={200}>
+		<Tooltip.Trigger>
+			{@render trigger()}
+		</Tooltip.Trigger>
+
+		<Tooltip.Content
+			sideOffset={8}
+			class="bg-base-100 rounded-lg border-dark-10 z-0 flex items-center justify-center border p-3"
+		>
+			{@render content()}
+		</Tooltip.Content>
+	</Tooltip.Root>
+</Tooltip.Provider>
